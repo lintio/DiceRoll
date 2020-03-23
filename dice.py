fa5977c8db484c640bc054d61c7c33b744472669
lintio <rmkayne@live.com>
2020-03-23 21:52:10 +0000

    98% complete

--- dice.py
+++ dice.py
@@ -1,17 +1,41 @@
-import random
-menuSelect = 'init'
-while menuSelect != '0':
+import random, os
+menuSelect = 1
+totalDice = 1
+diceType = 'D6'
+dieMax = 6
+os.system('cls' if os.name == 'nt' else 'clear')
+while menuSelect != 'q':
     rolls = 0
     dice = list()
-    print('Select the number of dice to roll between 1 and 6')
-    print('0 - Quit')
+    print('Current settings -> ' + str(totalDice) + ' ' + diceType + ' Dice <-')
+    print('1) Roll dice')
+    print('2) Change Numebr of dice')
+    print('3) Change Dice')
+    print('q) To quit')
     menuSelect = input('-> ')
     if menuSelect == '0':
         break
-    elif int(menuSelect) > 6:
-        continue
-    else:
-        totalDice = int(menuSelect)
+    elif menuSelect == '1' or menuSelect == '':
+        while rolls < totalDice:
+            dice.append(random.randint(1, dieMax))
+            rolls += 1
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print('your rolls are: ')
+        print(dice)
+    elif menuSelect == '2':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print('Select the number of dice to roll')
+        print('Enter 0 to go back')
+        menuSelect = input('-> ')
+        if menuSelect == '0':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            continue
+        else:
+            totalDice = int(menuSelect)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            continue
+    elif menuSelect == '3':
+        os.system('cls' if os.name == 'nt' else 'clear')
         print('Select the dice type to roll')
         print('1 - D4')
         print('2 - D6')
@@ -19,43 +43,37 @@ while menuSelect != '0':
         print('4 - D10')
         print('5 - D12')
         print('6 - D20')
-        print('0 - Quit')
+        print('Enter 0 to go back')
         menuSelect = input('-> ')
         if menuSelect == '0':
-            break
+            continue
         elif menuSelect == '1':
-            while rolls < totalDice:
-                dice.append(random.randint(1,4))
-                rolls += 1
-            print('your ' + str(totalDice) + ' D4 rolls:')
-            print(dice)
+            diceType = 'D4'
+            dieMax = 4
+            os.system('cls' if os.name == 'nt' else 'clear')
+            continue
         elif menuSelect == '2':
-            while rolls < totalDice:
-                dice.append(random.randint(1,6))
-                rolls += 1
-            print('your ' + str(totalDice) + ' D6 rolls:')
-            print(dice)
+            diceType = 'D6'
+            dieMax = 6
+            os.system('cls' if os.name == 'nt' else 'clear')
+            continue
         elif menuSelect == '3':
-            while rolls < totalDice:
-                dice.append(random.randint(1,8))
-                rolls += 1
-            print('your ' + str(totalDice) + ' D8 rolls:')
-            print(dice)
+            diceType = 'D8'
+            dieMax = 8
+            os.system('cls' if os.name == 'nt' else 'clear')
+            continue
         elif menuSelect == '4':
-            while rolls < totalDice:
-                dice.append(random.randint(1,10))
-                rolls += 1
-            print('your ' + str(totalDice) + ' D10 rolls:')
-            print(dice)
+            diceType = 'D10'
+            dieMax = 10
+            os.system('cls' if os.name == 'nt' else 'clear')
+            continue
         elif menuSelect == '5':
-            while rolls < totalDice:
-                dice.append(random.randint(1,12))
-                rolls += 1
-            print('your ' + str(totalDice) + ' D12 rolls:')
-            print(dice)
+            diceType = 'D12'
+            dieMax = 12
+            os.system('cls' if os.name == 'nt' else 'clear')
+            continue
         elif menuSelect == '6':
-            while rolls < totalDice:
-                dice.append(random.randint(1,20))
-                rolls += 1
-            print('your ' + str(totalDice) + ' D20 rolls:')
-            print(dice)
+            diceType = 'D20'
+            dieMax = 20
+            os.system('cls' if os.name == 'nt' else 'clear')
+            continue
